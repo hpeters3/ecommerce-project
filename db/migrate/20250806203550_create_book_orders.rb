397aaeba6ae858687c3627916146dca9588f3def
hpeters <hpeters3@academic.rrc.ca>
2025-08-06 15:39:23 -0500

    BookOrder Updates

--- db/migrate/20250806203550_create_book_orders.rb
+++ db/migrate/20250806203550_create_book_orders.rb
@@ -1,7 +1,7 @@
 class CreateBookOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :book_orders do |t|
-      t.string :order_id,
+      t.string :order_id
       t.string :book_id
 
       t.timestamps
