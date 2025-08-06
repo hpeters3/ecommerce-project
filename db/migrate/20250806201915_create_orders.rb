8dd006f73ba1b1da3d944f460b7b8804f6a93b97
hpeters <hpeters3@academic.rrc.ca>
2025-08-06 15:48:43 -0500

    Order Updates

--- db/migrate/20250806201915_create_orders.rb
+++ db/migrate/20250806201915_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :orders do |t|
-      t.string :customer_id
+      t.integer :customer_id, null: false
       t.string :book_id
       t.integer :books_ordered
       t.decimal :total_cost
