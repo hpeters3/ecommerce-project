8b4c2f831bd48a2a324b73d78f24033a3939ddec
hpeters <hpeters3@academic.rrc.ca>
2025-08-06 15:48:56 -0500

    BookOrder Updates

--- db/migrate/20250806203550_create_book_orders.rb
+++ db/migrate/20250806203550_create_book_orders.rb
@@ -1,10 +1,14 @@
 class CreateBookOrders < ActiveRecord::Migration[8.0]
   def change
     create_table :book_orders do |t|
-      t.string :order_id
-      t.string :book_id
+      t.integer :book_id, null: false
+      t.integer :order_id, null: false
 
       t.timestamps
     end
+
+    add_index :book_orders, [ :book_id, :order_id ], unique: true
+    add_foreign_key :book_orders, :books, column: :book_id
+    add_foreign_key :book_orders, :orders, column: :order_id
   end
 end
