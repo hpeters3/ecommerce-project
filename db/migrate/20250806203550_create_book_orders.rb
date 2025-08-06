1f429d9fa79358fd3d43c96cae8c74a036b32718
hpeters <hpeters3@academic.rrc.ca>
2025-08-06 15:36:10 -0500

    BookOrder Migration Added

---
+++ db/migrate/20250806203550_create_book_orders.rb
@@ -0,0 +1,10 @@
+class CreateBookOrders < ActiveRecord::Migration[8.0]
+  def change
+    create_table :book_orders do |t|
+      t.string :order_id,
+      t.string :book_id
+
+      t.timestamps
+    end
+  end
+end
